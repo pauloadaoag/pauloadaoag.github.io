88d92f99e7c5027f49f19b811aff54b1bffbf776
Paulo Adaoag <p@itemhound.com>
2014-03-12 01:02:28 +0800

    zoom in and out

--- js/main.js
+++ js/main.js
@@ -67,112 +67,131 @@ $.fn.slider = function(option, val) {
     })
 };
 
-$.fn.slider = function(){
-  
-  var tooltipStr = ('<div class="tooltip top in" style="bottom: 0px; left: 0px;"><div class="tooltip-arrow"></div><div class="tooltip-inner">here</div></div>');
-  var toolTips = [];
-  var slider = $('<div class="slider"></div></div>')
-  var leftEdge = $(this).position().left;
-  var topEdge = $(this).position().top;
-  console.log(leftEdge)
-  var tooltip = {}
-  tooltip.moving = false;
-  //tooltip.disableTextSelect()
-  // tooltip.on("mousedown", function(event){
-  //   console.log("tooltip clicked")
-  //   tooltip.moving = !tooltip.moving;
-  //   event.stopPropagation(); //Stop bubbling up to parent
-  // })
+$.fn.slider = function() {
+    var parent = $(this);
+    var tooltipStr = ('<div class="tooltip top in" style="bottom: 0px; left: 0px;"><div class="tooltip-arrow"></div><div class="tooltip-inner">drag</div></div>');
+    var toolTips = [];
+    var slider = $('<div class="slider"></div></div>')
+    var leftEdge = $(this).position().left;
+    var topEdge = $(this).position().top;
+    console.log(leftEdge)
+    var tooltip = {}
+    tooltip.moving = false;
+    //tooltip.disableTextSelect()
+    // tooltip.on("mousedown", function(event){
+    //   console.log("tooltip clicked")
+    //   tooltip.moving = !tooltip.moving;
+    //   event.stopPropagation(); //Stop bubbling up to parent
+    // })
 
-  // tooltip.on("mouseup", function(event){
-  //   if (tooltip.moving) tooltip.moving = false;
-  // })
+    // tooltip.on("mouseup", function(event){
+    //   if (tooltip.moving) tooltip.moving = false;
+    // })
 
 
-  var root = $(this)
-  $(this).on("mousemove", function(event){
-    
-    if (movingToolTip !== null){
-      var distanceToTop = (event.pageY - topEdge)
-      var left = $(this).position().left;
-      var width = (movingToolTip.width())
-      movingToolTip.css('left', (event.pageX - leftEdge - (width/2)) +"px")
-      if (distanceToTop < 30){
-        movingToolTip.css('bottom', "30px")
-        // for (var i = 0; i < ticks.length; i++){
-        //   ticks[i].css("left", 2*i * (100/12)+"%")
-        // }
-        slider.css("width","1200px")
-      }
-      if (distanceToTop > 70){
-        movingToolTip.css("bottom",bottom  + "px")    
-        // for (var i = 0; i < ticks.length; i++){
-        //   ticks[i].css("left", i * (100/12)+"%")
-        // }
-        slider.css("width","600px")
-      }
-      
+    var root = $(this)
+    $(this).on("mousemove", function(event) {
+        //console.log("x: %s - y: %s ", event.pageX, event.pageY)
+        if (movingToolTip !== null) {
+            var snapPos = (movingToolTip.attr("snapPos"))
+            var distanceToTop = (event.pageY - topEdge)
+            var left = $(this).position().left;
+            var width = (movingToolTip.width())
+            var leftMargin = slider.position().left
+            var posWithinParent = (event.pageX - leftEdge + (width/2)  - leftMargin )/slider.width();
+            //var newLeft = (event.pageX - leftEdge - (width / 2) + leftMargin)
+            var newLeft = (event.pageX - leftEdge - (width / 2) - leftMargin)
+            movingToolTip.css('left', newLeft + "px")
+
+            console.log("%s - leftMargin: %s posinparent:%s", newLeft, leftMargin, posWithinParent)
+            if (snapPos == "top"){
+              //slider.css("left",-(event.pageX - leftEdge)+"px")
+            }
+
+            if ((snapPos == "bottom")&&(distanceToTop < 30)) {
+                movingToolTip.css('bottom', "30px")
+                movingToolTip.attr("snapPos","top")
+                // for (var i = 0; i < ticks.length; i++){
+                //   ticks[i].css("left", 2*i * (100/12)+"%")
+                // }
+                debugger;
+                slider.css("width", "2400px")
+                var oldLeftMargin = slider.position().left;
+                var pol = event.pageX - leftEdge - (posWithinParent*2400) + width + oldLeftMargin;
+                //slider.css("left", -(posWithinParent*(2400) - event.pageX + leftEdge - oldLeftMargin)  +"px")
+                slider.css("left", pol  +"px")
+                leftMargin = slider.position().left
+                newLeft = (event.pageX - leftEdge - (width / 2) - leftMargin)
+                movingToolTip.css('left', newLeft + "px")
+                //movingToolTip.css("left", posWithinParent*100 + "%")
+                //movingToolTip.css("left", posWithinParent*100 + "%")
+                // console.log(posWithinParent*100)
+                // console.log(slider.position())
+
+            }
+            if ((snapPos == "top")&&(distanceToTop > 70)) {
+                movingToolTip.attr("snapPos","bottom")
+                movingToolTip.css("bottom", bottom + "px")
+                // for (var i = 0; i < ticks.length; i++){
+                //   ticks[i].css("left", i * (100/12)+"%")
+                // }
+                 posWithinParent = (event.pageX - leftEdge + (width/2))/2400;
+                movingToolTip.css("left", ((posWithinParent*600)) + "px")
+                slider.css("width", "600px")
+                slider.css("left","30px")
+            }
+
+        }
+        event.stopPropagation();
+    })
+
+    var ticks = [];
+
+    for (var i = 0; i < 24; i++) {
+        var toAppend = $("<div class='tick'></div>")
+        toAppend.text(i+1)
+        toAppend.css("left", i * (100 / 24) + "%");
+        ticks.push(toAppend)
+        slider.append(toAppend);
     }
-    event.stopPropagation(); 
-  })
 
-  var ticks = [];
+    var expand = function() {
 
-  for (var i = 0; i < 12; i++){
-    var toAppend = $("<div class='tick'></div>")
-    toAppend.css("left", i * (100/12) + "%");
-    ticks.push(toAppend)
-    slider.append(toAppend);
-  }
-  
-  var expand = function(){
+    }
 
-  }
-  
-  $(this).append(slider)
-  var initialXPos = 0;
-  var movingToolTip = null
-  var bottom = slider.height();
-  $(this).on('mousedown',function(data){
-    var newToolTip = $(tooltipStr);
+    $(this).append(slider)
+    var initialXPos = 0;
+    var movingToolTip = null
+    var bottom = slider.height();
+    $(this).on('mousedown', function(data) {
+        var newToolTip = $(tooltipStr);
 
-    toolTips.push(newToolTip)
-    newToolTip.css("left", data.offsetX+"px")
-    newToolTip.css("bottom", bottom+"px")
-    newToolTip.disableTextSelect();
-    newToolTip.on("mousedown", function(event){
-      movingToolTip = $(this)
-      event.stopPropagation(); //Stop bubbling up to parent
+        toolTips.push(newToolTip)
+        newToolTip.css("left", data.offsetX + "px")
+        newToolTip.css("bottom", bottom + "px")
+        newToolTip.attr("snapPos","bottom")
+        newToolTip.disableTextSelect();
+        newToolTip.on("mousedown", function(event) {
+            movingToolTip = $(this)
+            event.stopPropagation(); //Stop bubbling up to parent
+        });
+
+        slider.append(newToolTip);
     })
 
-
-
-    slider.append(newToolTip);
-    console.log(newToolTip.height())
-    
-    console.log(toolTips.length)
-    
-
-    //initialXPos = toolTips[toolTips.length - 1].position().top;
-  })
-  // })
-
-  // $(this).on('mouseup',function(){
-  //   console.log("mouseup")
-  //   if (tooltip.moving) tooltip.moving = false;
-  // })
-    $(this).on("mouseup", function(event){
-      movingToolTip = null;
-      event.stopPropagation(); //Stop bubbling up to parent
+    $(this).on("mouseup", function(event) {
+        movingToolTip = null;
+        event.stopPropagation(); //Stop bubbling up to parent
     })
-    $(this).on("mouseleave", function(event){
-      movingToolTip = null;
-      event.stopPropagation(); //Stop bubbling up to parent
+    $(this).on("mouseleave", function(event) {
+        movingToolTip = null;
+        event.stopPropagation(); //Stop bubbling up to parent
     })
-  // $(this).on('mouseleave',function(data){
-    
-  // })
+    // $(this).on('mouseleave',function(data){
+
+    // })
 
 }
 
 $('.tSlider').slider();
+$('.tSlider').disableTextSelect();
